3be683302a54b6b237d4537e404cd494a92dec40
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-06-18 21:15:44 +0200

    Use HTMX to add a digit

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    routing::{get, post},
+};
 use maud::{DOCTYPE, Markup, html};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/calc", post(calc));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn root() -> Markup {
     html! {
-        (DOCTYPE)
-        meta charset="utf-8";
-        form {
-            input type="button" value="1";
-            input type="button" value="2";
-            input type="text" name="output" value="";
-            label for="output" { "Resultado" };
+       (DOCTYPE)
+        head {
+            meta charset="utf-8";
+            script src="https://unpkg.com/htmx.org@2.0.4" {};
         }
+        button hx-post="/calc" hx-include="[name='result']" hx-target="[name='result']" hx-swap="textContent" name="digit" value="1" { "1" }
+        input name="result" type="text" { "" }
+    }
+}
+
+async fn calc() -> Markup {
+    html! {
+        "5"
     }
 }
