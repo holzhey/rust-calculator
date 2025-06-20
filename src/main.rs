76952e44d9694935dff9dd8993193d41d477d6fb
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-06-20 20:55:42 +0200

    Add buttons 0-9

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,9 @@ async fn root() -> Markup {
             meta charset="utf-8";
             script src="https://unpkg.com/htmx.org@2.0.4" {};
         }
-        button hx-post="/calc" hx-include="[name='result']" hx-target="[name='result']" hx-swap="outerHTML" name="digit" value="1" { "1" }
+        @for number in 0..10 {
+            button hx-post="/calc" hx-include="[name='result']" hx-target="[name='result']" hx-swap="outerHTML" name="digit" value=(number) { (number) };
+        }
         input name="result" type="text" value="";
     }
 }
