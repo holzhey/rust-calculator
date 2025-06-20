31c4ee5a5a06c502baa97414b913a1c4dcef8311
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-06-20 20:13:13 +0200

    Update works

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,13 @@ async fn root() -> Markup {
             meta charset="utf-8";
             script src="https://unpkg.com/htmx.org@2.0.4" {};
         }
-        button hx-post="/calc" hx-include="[name='result']" hx-target="[name='result']" hx-swap="textContent" name="digit" value="1" { "1" }
-        input name="result" type="text" { "" }
+        button hx-post="/calc" hx-include="[name='result']" hx-target="[name='result']" hx-swap="outerHTML" name="digit" value="1" { "1" }
+        input name="result" type="text" value="";
     }
 }
 
 async fn calc() -> Markup {
     html! {
-        "5"
+         input name="result" type="text" value="1";
     }
 }
