5567ae911627c74e4899664555048fc52e8e365f
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-09-11 21:34:45 +0200

    Refactored references

--- src/handler.rs
+++ src/handler.rs
@@ -2,8 +2,6 @@ use axum::Form;
 use maud::Markup;
 use serde::Deserialize;
 
-use crate::view::{output, page};
-
 #[derive(Deserialize, Debug)]
 pub struct Operation {
     result: String,
@@ -11,21 +9,21 @@ pub struct Operation {
     accumulator: String,
 }
 
-pub async fn page_handler() -> Markup {
-    page()
+pub async fn page() -> Markup {
+    crate::view::page()
 }
 
-pub async fn input_handler(Form(input): Form<Operation>) -> Markup {
-    let current_result = input.result.to_owned();
-    let current_acc = input.accumulator.to_owned();
-    let new_digit = input.action.to_owned();
+pub async fn input(Form(input): Form<Operation>) -> Markup {
+    let current_result = input.result;
+    let current_acc = input.accumulator;
+    let new_digit = input.action;
     let new_result = format!("{current_result}{new_digit}");
-    output(&new_result, &current_acc.to_string())
+    crate::view::output(&new_result, &current_acc.to_string())
 }
 
-pub async fn operation_handler(Form(operation): Form<Operation>) -> Markup {
+pub async fn operation(Form(operation): Form<Operation>) -> Markup {
     let current_result = operation.result.parse::<i32>().unwrap();
     let current_acc = operation.accumulator.parse::<i32>().unwrap();
     let new_result = current_acc + current_result;
-    output("0", &new_result.to_string())
+    crate::view::output("0", &new_result.to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use axum::{
     Router,
     routing::{get, post},
 };
-use handler::{input_handler, operation_handler, page_handler};
+use tokio::net::TcpListener;
 
 pub mod handler;
 pub mod view;
@@ -10,9 +10,9 @@ pub mod view;
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(page_handler))
-        .route("/input", post(input_handler))
-        .route("/operation", post(operation_handler));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+        .route("/", get(handler::page))
+        .route("/input", post(handler::input))
+        .route("/operation", post(handler::operation));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
